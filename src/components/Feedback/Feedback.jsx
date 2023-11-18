552698d91cff3502874e125a4a45a20aaa949da0
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2023-11-18 07:40:15 -0600

    feedback

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,29 +1,21 @@
-import { Component } from 'react';
-class Feedback extends Component {
-  state = {};
-
-  handleSubmit = evt => {
-    evt.preventDefault();
-
-    const form = evt.currentTarget;
-
-    const login = form.elements.login.value;
-    const password = form.elements.password.value;
-
-    console.log(login, password);
-    this.props.onSubmit({ login, password });
-    form.reset();
-  };
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <button type="submit">Good</button>
-        <button type="submit">Neutral</button>
-        <button type="submit">Bad</button>
-      </form>
-    );
-  }
-}
+const Feedback = ({ options, onLeaveFeedback }) => {
+  return (
+    <div>
+      {options.map(option => {
+        return (
+          <button
+            key={option}
+            type="submit"
+            onClick={() => {
+              onLeaveFeedback(option);
+            }}
+          >
+            {option}
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Feedback;
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,9 +1,21 @@
-import { Component } from 'react';
-class Statistics extends Component {
-  state = {};
-  render() {
-    return <h2>Statistics</h2>;
-  }
-}
+const Statistics = ({
+  good,
+  neutral,
+  bad,
+  total,
+  positivePercentage,
+  reset,
+}) => {
+  return (
+    <div>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
+      <p>Total: {total}</p>
+      <p>Positive Feedback: {positivePercentage} %</p>
+      <button onClick={reset}>Reset</button>
+    </div>
+  );
+};
 
 export default Statistics;
